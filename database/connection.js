f34dd2974d41d799617a6f280f9a42097e54c3f4
fguardati <fguardati@circostudio.com>
2026-04-05 17:59:25 -0300

    metodos de db con esquema de promesas

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,24 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: 'localhost',
     user: 'api',
     password: '12345678',
     database: 'todolistdb'
 });
 
-db.connect((err) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log('Base de datos conectada!');
+(
+    async () => {
+        try{
+            const connection = await db.getConnection();
+            console.log('Base de datos conectada!');
+            connection.release();
+        }
+
+        catch(e){
+            console.log('Error al conectar la base de datos: ', e);
+        }
     }
-});
+)();
 
 module.exports = db;
--- models/tasksModel.js
+++ models/tasksModel.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const db = require('../database/connection.js');
 
-function getTaskByUser(req,res){
+async function getTaskByUser(req,res){
     const userID = req.params.userid;
 
-    db.query(
-        'SELECT * FROM tasks AS T JOIN users AS U ON T.userID = U.userID WHERE U.userID = ?',
-        [userID],
-        (err,results,fields) => {
-            if(err){
-                throw new Error(err);
-            }
-            console.log(results);
+    try{
+      const [results] = await db.query(
+            `SELECT T.taskID,T.taskName,t.taskDescription,C.nameCat,T.expDate
+             FROM tasks AS T 
+             JOIN users AS U
+             JOIN categories AS C
+             ON T.userID = U.userID AND T.catID = C.idCat
+             WHERE U.userID = ?`,
+            [userID]
+        );
+        
+        if(results.length < 0){
+            res.status(204).end();
         }
-    );
 
-    res.status(200).send(); 
+        res.status(200).json({
+            results
+        });
+    }
+
+    catch(e){
+        res.status(500).json({
+            msg: 'Error al obtener las tareas.'
+        });
+        console.error(e);
+    }
+ 
 }
 
 module.exports = {
